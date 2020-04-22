5c06e22bd1fc1c3f83eb9fcf5af2049387cdf53d
bhokumar <bhoopendra.akgec@gmail.com>
2020-04-23 00:38:34 +0530

    Adding Handlebar Templates

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -2,23 +2,38 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHandleBars = require('express-handlebars');
+
 
 const app = express();
 
-app.set('view engine', 'pug');
+
+app.engine('handlebars', expressHandleBars({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+
+app.set('view engine', 'handlebars');
+
 app.set('views', 'views');
+
+/*
+// Enable this to use pug as Template Engine
+app.set('view engine', 'pug');
+*/
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.render('404', {pageTitle: 'Page not found'});
+    res.render('404', {
+        pageTitle: 'Page not found'
+    });
 });
 
 app.listen(3000);
--- nodejs-complete-guide/routes/admin.js
+++ nodejs-complete-guide/routes/admin.js
@@ -10,7 +10,14 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+    res.render('add-product',
+        {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        activeAddProducts: true,
+        productsCSS: true
+    })
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
